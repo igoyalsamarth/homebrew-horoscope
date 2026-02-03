31bfaf18bd7bafea3ecf9bfbdadf6ab629869e54
Samarth Goyal <samarth.goyal1999@gmail.com>
2026-02-03 18:09:28 +0530

    fix: update formula url

--- Formula/horoscope.rb
+++ Formula/horoscope.rb
@@ -3,10 +3,10 @@ class Horoscope < Formula
   homepage "https://github.com/igoyalsamarth/horoscope-cli"
 
   if Hardware::CPU.arm?
-    url "https://github.com/igoyalsamarth/horoscope-cli/releases/download/v1.0.0/horoscope"
+    url "https://github.com/igoyalsamarth/horoscope-cli/releases/download/1.0.0/horoscope"
     sha256 "sha256:7d2600a763bce5741464f8c689ad86f28419a8d987263f4a78ea4ad282156bab"
   else
-    url "https://github.com/igoyalsamarth/horoscope-cli/releases/download/v1.0.0/horoscope-intel"
+    url "https://github.com/igoyalsamarth/horoscope-cli/releases/download/1.0.0/horoscope-intel"
     sha256 "sha256:36f8c3785c10808c0c03b61d66161ef46cfd5f09b38436c583e6740f24ae6499"
   end
 
